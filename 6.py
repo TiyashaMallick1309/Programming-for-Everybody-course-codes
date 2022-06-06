2560d51c77f2df86d0030b281dccc83cb1d4a029
TiyashaMallick1309 <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-06 20:48:35 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,10 @@
+#Write a function, accum, that takes a list of integers as input and returns the sum of those integers.
+
+def accum(l):
+    a=0
+    for i in l:
+        a+=i
+    return a
+lst=[]
+for j in lst:
+    j=int(input())
