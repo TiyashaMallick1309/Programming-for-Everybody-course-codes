30341041384d6732e6e9b4ea1de3986f379c8647
TiyashaMallick1309 <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-07 03:14:56 +0530

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,5 @@
+"""Sort the following string alphabetically, from z to a, and assign it to the variable sorted_letters."""
+
+sorted_letters=""
+letters = "alwnfiwaksuezlaeiajsdl"
+sorted_letters=sorted(letters,reverse=True)
