7ff162ccdd1c08d129b25500f13f3f12a3fc777e
TiyashaMallick1309 <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-06 17:54:38 +0530

    Update Ass3-1.py

--- Ass3-1.py
+++ Ass3-1.py
@@ -1,5 +1,5 @@
 """Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
-Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. 
+Pay the hourly rate for the hours up to 40 & 1.5 times the hourly rate for all hours worked above 40 hours. 
 You should use input to read a string and float() to convert the string to a number."""
 
 hrs = input("Enter Hours:")
