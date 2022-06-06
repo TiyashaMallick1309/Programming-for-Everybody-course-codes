dc3f7608e832dd0d9c249045a611f6d916091b2b
TiyashaMallick1309 <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-07 02:00:57 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,13 @@
+"""Below, we have provided a list of strings called nums. 
+Write a function called last_char that takes a string as input, and returns only its last character. 
+Use this function to sort the list nums by the last digit of each number, from highest to lowest, 
+and save this as a new list called nums_sorted."""
+
+nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+
+def last_char(a):
+    for i in a:
+        return a[-1]
+
+nums_sorted =sorted(nums,reverse=True,key=last_char)
+
