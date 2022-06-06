25cf28e025c48067e0de7332cd9d34a30dda3ece
TiyashaMallick1309 <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-07 02:04:28 +0530

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,7 @@
+"""sort the list nums based on the last digit of each number from highest to lowest. 
+However, now you should do so by writing a lambda function. Save the new list as nums_sorted_lambda. """
+
+nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+
+nums_sorted_lambda =sorted(nums,reverse=True,key=lambda a: a[-1])
+
