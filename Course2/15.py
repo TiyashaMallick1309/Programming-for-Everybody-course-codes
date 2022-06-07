d4de03638f325af8704b0e20badd2e38d0eea2df
TiyashaMallick1309 <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-07 19:00:31 +0530

    Update and rename 20.py to Course2/15.py

--- Course2/15.py
+++ Course2/15.py
@@ -1,4 +1,4 @@
-"""sort the list nums based on the last digit of each number from highest to lowest. 
+"""Sort the list nums based on the last digit of each number from highest to lowest. 
 However, now you should do so by writing a lambda function. Save the new list as nums_sorted_lambda. """
 
 nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
