449657003eda270e6f5091fbbbbfe55464e0e156
Tiyasha Mallick <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-08 21:08:50 +0530

    Create 10.py

---
+++ Course5/10.py
@@ -0,0 +1,6 @@
+"""For each character in the string saved in ael, append that character to a list that should be saved in a variable app."""
+
+app=[]
+ael = "python!"
+for i in ael:
+    app.append(i)
