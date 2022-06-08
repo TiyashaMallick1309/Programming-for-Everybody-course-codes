a35fac6fbb8e93f123cc9c264b6744c1bca40b39
Tiyasha Mallick <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-08 18:40:21 +0530

    Update 1.py

--- Course5/1.py
+++ Course5/1.py
@@ -1,4 +1,6 @@
-"""Write code that combines the following variables so that the sentence “You are doing a great job, keep it up!” is assigned to the variable message. Do not edit the values assigned to by, az, io, or qy."""
+"""Write code that combines the following variables so that the sentence 
+“You are doing a great job, keep it up!” is assigned to the variable message. 
+Do not edit the values assigned to by, az, io, or qy."""
 
 by = "You are"
 az = "doing a great "
