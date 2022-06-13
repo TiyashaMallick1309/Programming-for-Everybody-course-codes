2429826ad5056a2a130100979239525dc5011b8f
Tiyasha Mallick <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-14 01:29:28 +0530

    Create 10.py

---
+++ Course3/10.py
@@ -0,0 +1,3 @@
+"""The program will prompt for a URL, read the XML data from that URL using urllib and 
+then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file."""
+
