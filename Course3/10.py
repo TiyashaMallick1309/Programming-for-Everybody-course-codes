8b229d88766ef0ee0be0d8609557e27b0a1bb0a1
Tiyasha Mallick <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-14 15:27:07 +0530

    Update 10.py

--- Course3/10.py
+++ Course3/10.py
@@ -1,3 +1,24 @@
 """The program will prompt for a URL, read the XML data from that URL using urllib and 
 then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file."""
 
+import urllib.request
+import urllib.parse
+import urllib.error
+import xml.etree.ElementTree as et
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+html = urllib.request.urlopen(
+    'http://py4e-data.dr-chuck.net/comments_1567349.xml', context=ctx).read()
+sum = 0
+c = 0
+s = et.fromstring(html)
+l = s.findall('.//count')
+for i in l:
+    c += 1
+    sum += int(i.text)
+print(c, "\n", sum)
