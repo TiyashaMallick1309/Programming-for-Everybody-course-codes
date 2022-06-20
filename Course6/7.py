caffe965ab623480c549e30bf999a38d552e8131
Tiyasha Mallick <92668444+TiyashaMallick1309@users.noreply.github.com>
2022-06-20 22:45:12 +0530

    Create 7.py

---
+++ Course6/7.py
@@ -0,0 +1,4 @@
+"""Using filter, filter lst so that it only contains words containing the letter “o”. Assign to variable lst2. """
+
+lst = ["witch", "halloween", "pumpkin", "cat", "candy", "wagon", "moon"]
+lst2=list(filter(lambda a:"o" in a,lst))
